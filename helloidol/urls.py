8a2f54b59e506f3855af40bff3b19d4d7d8ff37a
ksh1024 <ksh251024@gmail.com>
2024-03-25 11:08:03 +0900

    playground/hello/ => hello/, playground/hello_html/ => playground/ -> hello_html/

--- helloidol/urls.py
+++ helloidol/urls.py
@@ -15,12 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-import playground.views
 
 urlpatterns = [
-    path('playground/hello/', playground.views.say_hello, name='playground_hello'),
-    path('playground/hello_html/', playground.views.say_hello_html, name='playground_hello_html'),
+    #함수뒤 괄호가 없음
+    path('playground/', include('playground.urls')),
+    # path('playground/hello/', playground.views.say_hello, name='playground_hello'),
+    # path('playground/hello_html/', playground.views.say_hello_html, name='playground_hello_html'),
     path('admin/', admin.site.urls),
 ]
