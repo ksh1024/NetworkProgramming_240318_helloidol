98d0921d4db28c14f552b488159741106dda579b
ksh1024 <ksh251024@gmail.com>
2024-03-25 12:16:57 +0900

    views -> templates에 context 전달

--- playground/views.py
+++ playground/views.py
@@ -5,8 +5,12 @@ def say_hello(reguest):
     return HttpResponse("Hello World!")
 
 def say_hello_html(request):
-    return render(request, 'playground/hello.html')
+    return render(request, 'playground/hello.html', {'name' : '지우야 ','greeting': '안녕'})
 
 def say_bye_html(request):
-    return render(request, 'playground/bye.html')
+    context ={
+        'singer' : '아리아나 그란데',
+        'title' : 'bye',
+    }
+    return render(request, 'playground/bye.html', context=context)
 
